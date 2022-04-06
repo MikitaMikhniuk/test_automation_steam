778c099b2c6f37fec86d646df5ea2547568ed4e6
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-04-06 21:20:35 +0300

    moved to shared resources and renamed json reader

--- framework/utils/config_reader.py
+++
@@ -1,9 +0,0 @@
-import json
-
-CONFIG_PATH = "framework\\resources\\factory_config.json"
-
-
-def get_factory_config():
-    config_file = open(CONFIG_PATH)
-    factory_config = json.load(config_file)
-    return factory_config
