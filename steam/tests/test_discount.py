132f1d4e8a58542601e446370583f8c70bc1a086
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-04-06 21:36:51 +0300

    updated to exclude (driver)

--- steam/tests/test_discount.py
+++ steam/tests/test_discount.py
@@ -8,25 +8,26 @@ from steam.page_objects.age_page import AgePage
 
 test_data_path = r"steam\resources\test_data.json"
 
+
 def test_max_discount(setup):
     driver = setup
     test_data = get_json(test_data_path)
-    main_page = MainPage(driver)
+    main_page = MainPage()
     main_page.check_for_current_lang(
         test_data["DESIRED_LANG_CODE"], test_data["DESIRED_LANG_FULL"]
     )
     # main_page.verify_current_page_by_url(get_test_data["START_URL"])
     main_page.navigate_menu(
-        get_label(driver, "Categories"),
-        get_label(driver, "Action"),
+        get_label("Categories"),
+        get_label("Action"),
     )
 
-    category_page = CategoryPage(driver)
-    category_page.verify_category_page(get_label(driver, "Action"))
+    category_page = CategoryPage()
+    category_page.verify_category_page(get_label("Action"))
 
     app_id = category_page.click_on_max_discount_game()
 
-    age_page = AgePage(driver)
+    age_page = AgePage()
     age_page.wait_for_age_verification_page(
         app_id,
         test_data["DOB_DAY"],
@@ -34,9 +35,9 @@ def test_max_discount(setup):
         test_data["DOB_YEAR"],
     )
 
-    app_page = AppPage(driver)
+    app_page = AppPage()
     app_page.verify_current_app_page(app_id)
     app_page.navigate_to_download_page()
 
-    download_page = DownloadPage(driver)
+    download_page = DownloadPage()
     download_page.download_installer(test_data["FILE_NAME"])
