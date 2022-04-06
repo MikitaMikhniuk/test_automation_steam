7fd7b36e43b4aaf6305bfe069f0ea5d3519159f7
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-04-06 21:22:29 +0300

    updated config route + setup method

--- steam/tests/conftest.py
+++ steam/tests/conftest.py
@@ -1,22 +1,14 @@
-import os
 import pytest
-import json
-from framework.utils.driver_factory import DriverFactory
 from framework.utils.browser import Browser
+from framework.utils.json_reader import get_json
 
-CONFIG_PATH = "test_data.json"
+CONFIG_PATH = "steam\\resources\\factory_config.json"
+
 
 @pytest.fixture(scope="session")
 def setup():
-    driver_instance = DriverFactory()
-    driver = driver_instance.set_up()
-    yield driver
-    Browser.tear_down(driver)
-    
-@pytest.fixture(scope="session")
-def get_test_data():
-    os.chdir("../..")
-    test_data_file = open(CONFIG_PATH, encoding="UTF-8")
-    test_data = json.load(test_data_file)
-    os.chdir("../..")
-    return test_data
+    browser = Browser()
+    browser.maximize_window()
+    browser.navigate(get_json(CONFIG_PATH)["START_URL"])
+    yield browser.driver
+    browser.tear_down(browser.driver)
