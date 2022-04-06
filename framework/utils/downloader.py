fa1da9d596f07981bc661dc374fde2cdd92e9eda
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-04-06 21:23:21 +0300

    new config reader

--- framework/utils/downloader.py
+++ framework/utils/downloader.py
@@ -1,7 +1,9 @@
 import os
 import time
-from framework.utils import config_reader
+from framework.utils import json_reader
 
+CONFIG_PATH = r"steam\resources\factory_config.json"
+factory_config = json_reader.get_json(CONFIG_PATH)
 
 def set_up_download_folder():
     """
@@ -11,7 +13,7 @@ def set_up_download_folder():
 
     Returns (str) -> download path
     """
-    factory_config = config_reader.get_factory_config()
+    project_dir = os.getcwd()
     os.chdir(factory_config["DEFAULT_DOWNLOAD_PATH"])
     folder_name = factory_config["BROWSER"] + "_" + \
         'test_run_' + time.strftime("%d.%m.%Y^%H_%M_%S")
@@ -20,6 +22,7 @@ def set_up_download_folder():
     print(
         f"Files will be downloaded to {factory_config['DEFAULT_DOWNLOAD_PATH']} > {folder_name}")
     default_download_path = os.getcwd()
+    os.chdir(project_dir)
     return default_download_path
 
 
@@ -30,7 +33,6 @@ def wait_for_download_finish(file_name):
     Input -> Full file name with its extension (str).
     """
     print(os.getcwd())
-    factory_config = config_reader.get_factory_config()
     wait_sec = int(factory_config["DOWNLOAD_WAIT"])
     i = 0
     while i < wait_sec:
