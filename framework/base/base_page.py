2a3d0a274386d3e3e121f2bb4abb8101dc9c203e
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-04-06 21:23:57 +0300

    cleaned a little bit

--- framework/base/base_page.py
+++ framework/base/base_page.py
@@ -6,14 +6,7 @@ class BasePage:
     # искать решение для уборки driver отсюда
     def __init__(self, driver):
         self.driver = driver
-
-    # def __init__(self, element_locator, text):
-    #     self.verify_opened_page(element_locator, text)
-
-    # @staticmethod
-    # def verify_opened_page(element_locator, text):
-
-    
+           
     def get_locator_with_replaced_xpath(self, input_xpath, replace_what, replace_to):
         locator_xpath = input_xpath.replace(replace_what, replace_to)
         return locator_xpath
