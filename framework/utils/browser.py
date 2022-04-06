022f9c0d4ecbbd40a363759ff34184a02847cdac
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-04-06 21:21:20 +0300

    updated as expected

--- framework/utils/browser.py
+++ framework/utils/browser.py
@@ -1,17 +1,9 @@
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from framework.utils.downloader import set_up_download_folder
-from framework.utils import config_reader
+from framework.utils.driver_factory import DriverFactory
 
 
 class Browser:
 
-    def __init__(self, driver):
-        self.driver = driver
+    driver = DriverFactory().get_driver()
 
     def navigate(self, url):
         self.driver.get(url)
@@ -30,47 +22,11 @@ class Browser:
     def page_reload(self):
         self.driver.execute_script("window.location.reload()")
 
-    @staticmethod
-    def browser_setup():
-        factory_config = config_reader.get_factory_config()
-        if factory_config["USE_DOWNLOADER"] == "True":
-            default_download_path = set_up_download_folder()
-        else:
-            default_download_path = "\\"
-        # весь иф сунуть в driver_factory, а потом юзать driver_factory тут
-        if factory_config["BROWSER"] == "Chrome":
-            options = webdriver.ChromeOptions()
-            options.add_argument("start-maximized")
-            options.add_argument("--safebrowsing-disable-download-protection")
-            file_prefs = {"download.default_directory": default_download_path,
-                          "download.prompt_for_download": False,
-                          "safebrowsing.enabled": True}
-            options.add_experimental_option("prefs", file_prefs)
-            if factory_config["HEADLESS_MODE"] == "True":
-                options.add_argument("--headless")
-            s = ChromeService(ChromeDriverManager().install())
-        elif factory_config["BROWSER"] == "Firefox":
-            options = FirefoxOptions()
-            options.set_preference("browser.download.folderList", 2)
-            options.set_preference(
-                "browser.download.dir", default_download_path)
-            options.set_preference("browser.download.useDownloadDir", True)
-            options.set_preference(
-                "browser.download.viewableInternally.enabledTypes", "")
-            options.set_preference(
-                "browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
-            options.set_preference(
-                "browser.download.manager.showWhenStarting", False)
-            if factory_config["HEADLESS_MODE"] == "True":
-                options.headless = True
-            s = FirefoxService(GeckoDriverManager().install())
-        return s, options
+    def maximize_window(self):
+        self.driver.maximize_window()
 
     @staticmethod
     def tear_down(driver):
-        """
-        Driver tear down.
-        """
         if (driver != None):
             print("Cleanup of test environment!")
             driver.close()
--- framework/utils/driver_factory.py
+++ framework/utils/driver_factory.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
-from framework.utils.browser import Browser
-from framework.utils import config_reader
+from framework.utils import json_reader
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from framework.utils.downloader import set_up_download_folder
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
+CONFIG_PATH = "steam\\resources\\factory_config.json"
 
 class DriverFactory:
     """
@@ -9,15 +15,38 @@ class DriverFactory:
 
     """
     @staticmethod
-    def set_up():
-        """
-        Driver set up.
-        """
-        factory_config = config_reader.get_factory_config()
-        s, options = Browser.browser_setup()
+    def get_driver():
+        factory_config = json_reader.get_json(CONFIG_PATH)
+        if factory_config["USE_DOWNLOADER"] == "True":
+            default_download_path = set_up_download_folder()
+        else:
+            default_download_path = "\\"
         if factory_config["BROWSER"] == "Chrome":
+            options = webdriver.ChromeOptions()
+            options.add_argument("--safebrowsing-disable-download-protection")
+            file_prefs = {"download.default_directory": default_download_path,
+                          "download.prompt_for_download": False,
+                          "safebrowsing.enabled": True}
+            options.add_experimental_option("prefs", file_prefs)
+            if factory_config["HEADLESS_MODE"] == "True":
+                options.add_argument("--headless")
+            s = ChromeService(ChromeDriverManager().install())
             driver = webdriver.Chrome(service=s, options=options)
         elif factory_config["BROWSER"] == "Firefox":
+            options = FirefoxOptions()
+            options.set_preference("browser.download.folderList", 2)
+            options.set_preference(
+                "browser.download.dir", default_download_path)
+            options.set_preference("browser.download.useDownloadDir", True)
+            options.set_preference(
+                "browser.download.viewableInternally.enabledTypes", "")
+            options.set_preference(
+                "browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+            options.set_preference(
+                "browser.download.manager.showWhenStarting", False)
+            if factory_config["HEADLESS_MODE"] == "True":
+                options.headless = True
+            s = FirefoxService(GeckoDriverManager().install())
             driver = webdriver.Firefox(service=s, options=options)
         else:
             raise Exception("I don't know such driver yet!")
