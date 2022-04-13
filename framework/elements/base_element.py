2309b26a7210cf225673676159e6ebed20724676
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-04-13 13:18:09 +0300

    waits added

--- framework/elements/base_element.py
+++ framework/elements/base_element.py
@@ -1,17 +1,29 @@
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from framework.utils import json_reader
+
+CONFIG_PATH = "steam\\resources\\factory_config.json"
+config = json_reader.get_json(CONFIG_PATH)
 
 
 class BaseElement:
+
+    timeout = config["DEFAULT_WAIT_TIME"]
+
     def __init__(self, driver, element):
         self.driver = driver
         self.element = element
+        self.wait = WebDriverWait(self.driver, self.timeout)
 
     def click(self):
+        self.wait.until(EC.element_to_be_clickable(self.element))
         self.element.click()
 
     def click_and_wait(self):
         self.click()
         # move to Browser
+        self.wait.until(EC.staleness_of(self.element))
         page_state = self.driver.execute_script("return document.readyState;")
         try:
             assert page_state == "complete"
@@ -26,6 +38,7 @@ class BaseElement:
         actions.move_to_element(self.element)
 
     def send_keys(self, keys):
+        self.wait.until(EC.element_to_be_clickable(self.element))
         self.element.send_keys(keys)
 
     def get_text(self):
